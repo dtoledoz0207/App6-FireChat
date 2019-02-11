4c3e00e274ca976af2d8b80c2b7dd60317a7d68b
David Toledo <dtoledoz0207@gmail.com>
2019-02-11 13:55:04 -0600

    messagesInDivbox-Added

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,23 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {ChatService} from '../../providers/chat.service';
+
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styles: []
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
 
   mensaje:string = "";
+  elemento:any;
 
   constructor(public _cs:ChatService) {
-    this._cs.cargarMensajes().subscribe( (mensajes:any[]) => {
-      console.log(mensajes);
+    this._cs.cargarMensajes().subscribe( () => {
+
+      setTimeout(() => {
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      }, 20);
+      
     });
+  }
+
+  ngOnInit(){
+    this.elemento = document.getElementById('app-mensajes');
   }
 
   enviarMensaje(){
     console.log(this.mensaje);
+
+    if(this.mensaje.length === 0){
+      return;
+    }else{
+      this._cs.agregarMensaje(this.mensaje).then( () => {
+        console.log("Mensaje enviado");
+        this.mensaje = "";
+      }).catch( (err) => {
+        console.log("Error al enviar", err);
+      });
+    }
+
   }
 
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
+import {Mensaje} from '../interface/mensaje.interface';
+import {map} from 'rxjs/operators';
+
 @Injectable()
 export class ChatService {
 
-  private itemsCollection: AngularFirestoreCollection<any>;
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
-  public chats:any[] = [];
+  public chats:Mensaje[] = [];
 
   constructor(private afs: AngularFirestore) { }
 
   cargarMensajes(){
-    this.itemsCollection = this.afs.collection<any>('chats');
-    return this.itemsCollection.valueChanges();
+    this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref.orderBy('fecha','desc').limit(5));
+    return this.itemsCollection.valueChanges().pipe( map( (mensajes:Mensaje[]) => {
+
+      this.chats = [];
+
+      for (let mensaje of mensajes){
+        this.chats.unshift(mensaje);
+      }
+
+      return this.chats;
+    }));
+  }
+
+  agregarMensaje(texto:string){
+
+    // Falta del uid del usuario
+    let mensaje:Mensaje = {
+      nombre: "Demo",
+      mensaje: texto,
+      fecha: new Date().getTime()
+    }
+
+    return this.itemsCollection.add(mensaje);
+
   }
 
 }
