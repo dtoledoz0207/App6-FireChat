3e9fd8582929bf1d39416af2e15877ca8726d441
David Toledo <dtoledoz0207@gmail.com>
2019-02-12 12:16:03 -0600

    GoogleAuthentication-Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,17 @@ import { environment } from '../environments/environment';
 
 // COMPONENTS
 import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
 
 // SERVICIOS
 import {ChatService} from './providers/chat.service';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
